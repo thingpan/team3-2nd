a425616e309b35767db8302e07b0893af88cef80
WonJoon <sldisk35@nate.com>
2023-12-14 11:04:51 +0900

    매치뷰 사진 불러오기(근데 각자 컴퓨터에서만 가능)

---
+++ src/main/java/com/sp/team32ndproject/common/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.sp.team32ndproject.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+	
+	@Value("${download.file-path}")
+	private String filePath; 
+	@Value("${download.resource-url}")
+	private String resourceUrl;
+	
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler(resourceUrl)
+		.addResourceLocations(filePath);
+	}
+}
