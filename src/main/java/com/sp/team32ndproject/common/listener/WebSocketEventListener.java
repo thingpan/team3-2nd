9774755f37c887d5f8a6c55513e3e128a40950ee
lee <audrhks1618@naver.com>
2024-01-22 15:41:48 +0900

    메세지

--- src/main/java/com/sp/team32ndproject/common/listener/WebSocketEventListener.java
+++ src/main/java/com/sp/team32ndproject/common/listener/WebSocketEventListener.java
@@ -1,9 +1,17 @@
 package com.sp.team32ndproject.common.listener;
 
+
+import org.apache.http.HttpHeaders;
+import org.apache.logging.log4j.message.SimpleMessage;
 import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
+
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,10 +21,17 @@ public class WebSocketEventListener {
 
 	@EventListener
 	public void connectionListener(SessionConnectedEvent evt) {
+		StompHeaderAccessor sha = StompHeaderAccessor.wrap(evt.getMessage());
+		GenericMessage<?> gm =(GenericMessage<?>)sha.getHeader(SimpMessageHeaderAccessor.CONNECT_MESSAGE_HEADER);
+		SimpMessageHeaderAccessor smha =SimpMessageHeaderAccessor.wrap(gm);
+		int uiNum =Integer.parseInt(smha.getFirstNativeHeader("uiNum"));
+		String token =smha.getFirstNativeHeader(HttpHeaders.AUTHORIZATION);
 		log.info("connection => {}",evt);
+		log.info("uiNum => {}",uiNum);
+		log.info("token => {}",token);
 	}
 	@EventListener
-	public void disconnectionListener(SessionConnectedEvent evt) {
+	public void disconnectionListener(SessionDisconnectEvent evt) {
 		log.info("disconnection => {}",evt);
 	}
 }
