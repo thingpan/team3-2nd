aab13c299ca2787dfa647276a1c8d0662ea87406
lee <audrhks1618@naver.com>
2024-02-06 11:31:35 +0900

    소켓

--- src/main/java/com/sp/team32ndproject/common/listener/WebSocketEventListener.java
+++ src/main/java/com/sp/team32ndproject/common/listener/WebSocketEventListener.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -35,44 +36,35 @@ public class WebSocketEventListener {
 	public static List<Integer> uiNums = Collections.synchronizedList(new ArrayList<>());
 	public static List<String> sessionIds = Collections.synchronizedList(new ArrayList<>());
 	public static List<UserInfoVO> users = Collections.synchronizedList(new ArrayList<>());
+	public static Map<String,Integer> connectedMap=Collections.synchronizedMap(new HashMap<>());
 	private final SimpMessagingTemplate smt;
 	private final UserInfoService userService;
 
+
 	@EventListener
 	public void connectionListener(SessionConnectedEvent evt) {
-		if (users == null || users.isEmpty()) {
-			List<UserInfoVO> tmpUsers = userService.selectUserInfos(null);
-			users = Collections.synchronizedList(tmpUsers);
-		}
 		StompHeaderAccessor sha = StompHeaderAccessor.wrap(evt.getMessage());
 		GenericMessage<?> gm = (GenericMessage<?>) sha.getHeader(SimpMessageHeaderAccessor.CONNECT_MESSAGE_HEADER);
 		SimpMessageHeaderAccessor smha = SimpMessageHeaderAccessor.wrap(gm);
 		int uiNum = Integer.parseInt(smha.getFirstNativeHeader("uiNum"));
-		String sessionId = smha.getSessionId();
-		String loginDate = DateUtil.getToDate();
-		
-		for (UserInfoVO user : users) {
-			if (user.getUiNum() == uiNum) {
-				user.setSessionId(sessionId);
-				user.setLogin(true);
-				user.setLoginDate(loginDate);
-			}
-		}
-		smt.convertAndSend("/topic/enter-chat", users);
+		UserInfoVO userInfoVO = new UserInfoVO();
+		userInfoVO.setUiNum(uiNum);
+		UserInfoVO connectedUser =userService.selectUserInfoByUiNum(userInfoVO);
+		String sessionId =sha.getSessionId();
+		connectedMap.put(sessionId,uiNum);
+		connectedUser.setLogin(true);
+		smt.convertAndSend("/topic/enter-chat", connectedUser);
 	}
-
 	@EventListener
 	public void disconnectionListener(SessionDisconnectEvent evt) {
 		StompHeaderAccessor sha = StompHeaderAccessor.wrap(evt.getMessage());
 		String sessionId = sha.getSessionId();
+		int uiNum =connectedMap.get(sessionId);
+		UserInfoVO userInfoVO = new UserInfoVO();
+		userInfoVO.setUiNum(uiNum);
+		UserInfoVO connectedUser =userService.selectUserInfoByUiNum(userInfoVO);
+		smt.convertAndSend("/topic/enter-chat", connectedUser);
 		
-		for (UserInfoVO user : users) {
-			if (sessionId.equals(user.getSessionId())) {
-				user.setSessionId(null);
-				user.setLogin(false);
-			}
-		}
-		smt.convertAndSend("/topic/enter-chat", users);
 	}
 
 	@EventListener // 구독 할때
