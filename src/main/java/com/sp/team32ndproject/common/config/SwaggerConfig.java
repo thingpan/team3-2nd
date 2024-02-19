4bc3f63b1d7f1b9ac4dcac8209b02e6a4261c66c
WonJuni <sldisk35@nate.com>
2024-02-19 14:18:44 +0900

    스웩 췍 스웩췌

---
+++ src/main/java/com/sp/team32ndproject/common/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.sp.team32ndproject.common.config;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+public class SwaggerConfig {
+
+	@Bean
+	Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.ignoredParameterTypes(AuthenticationPrincipal.class)
+				.ignoredParameterTypes(Pageable.class)
+				.useDefaultResponseMessages(false)
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.project.secu"))
+				.paths(PathSelectors.any())
+				.build();
+	}
+	
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("DevelopSpace Rest API Documetation")
+				.description("Develop Spave API 명세")
+				.version("0.1")
+				.build();
+	}
+}
