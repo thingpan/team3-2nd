05c26badaa567983239ebe684e730d1e6d164690
WonJuni <sldisk35@nate.com>
2024-02-19 14:44:33 +0900

    이왕균 엄
    청 멋있다

--- src/main/java/com/sp/team32ndproject/common/config/SwaggerConfig.java
+++ src/main/java/com/sp/team32ndproject/common/config/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.sp.team32ndproject.common.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +12,11 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -36,5 +43,22 @@ public class SwaggerConfig {
 				.description("Develop Spave API 명세")
 				.version("0.1")
 				.build();
+	}
+	
+	private ApiKey apiKey() {
+		return new ApiKey("JWT", "jwt", "header");
+	}
+	
+	private SecurityContext securityContext() {
+		return springfox.documentation.spi.service.
+				contexts.SecurityContext.builder().
+				securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+	}
+	
+	List<SecurityReference> defaultAuth(){
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
 	}
 }
