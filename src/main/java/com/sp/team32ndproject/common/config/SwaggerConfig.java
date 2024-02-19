6398718ddb5653128b4cc5fe24c9cd0c0deb7546
WonJuni <sldisk35@nate.com>
2024-02-19 14:33:09 +0900

    이명관은 네~모에 현질을 했다~

--- src/main/java/com/sp/team32ndproject/common/config/SwaggerConfig.java
+++ src/main/java/com/sp/team32ndproject/common/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.sp.team32ndproject.common.config;
 
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -10,7 +11,10 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
 	@Bean
